16a5d3cc52ad104ef946f6e4490dbacfdf5c8701
Rohit Kumar <Rohit.kumar1814.rk@gmail.com>
2025-02-25 16:31:46 -0500

    added

--- components/skill/controller.js
+++ components/skill/controller.js
@@ -20,7 +20,7 @@ const addNewSkill = async (request, response) => {
     response.redirect("../list",);
 }
 const getSkillAPI = async (request, response) => {
-    let skillList = await skillModel.getSkills();
+    let skillList = await skillModal.getSkills();
     response.json(skillList);
   };
 const deleteSkillById = async (request, response) => {
